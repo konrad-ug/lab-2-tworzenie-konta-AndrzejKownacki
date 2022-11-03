2ff6a96c0edaa82527f3712f2ba15b7eca906267
Andrzej Kownacki <akjjpol@gmail.com>
2022-11-03 17:42:06 +0100

    Start Historii

---
+++ app/tests/test_historia.py
@@ -0,0 +1,17 @@
+import unittest
+
+from ..Konto import Konto
+
+class TestKsiegowanie(unittest.TestCase):
+    imie = "Dariusz"
+    nazwisko = "Testowy"
+    pesel= "13467524635"
+
+
+    def test_historia(self):
+        konto = Konto(self.imie, self.nazwisko, self.pesel, None)
+        konto.saldo = 3000
+        konto.zaksieguj_przelew_przychodzacy(200)
+        konto.zaksieguj_przelew_wychodzacy(300)
+        konto.przelew_ekspresowy(400)
+        
