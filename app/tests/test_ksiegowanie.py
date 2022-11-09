fc3b76e3be7513564d11ba29b5a4fce510888dc1
Andrzej <akjjpol@gmail.com>
2022-11-09 21:41:37 +0100

    Testy do przelewow przychodzacych + historia

--- app/tests/test_ksiegowanie.py
+++ app/tests/test_ksiegowanie.py
@@ -10,13 +10,20 @@ class TestKsiegowanie(unittest.TestCase):
     def test_udany_przelew_wychodzacy(self):
         konto = Konto(self.imie, self.nazwisko, self.pesel, None)
         konto.saldo = 1000
-        konto.zaksieguj_przelew(200)
+        konto.zaksieguj_przelew_wychodzacy(200)
 
         self.assertEqual(konto.saldo, 800, "Udało się wykonać przelew")
 
     def test_nieudany_przelew_wychodzacy(self):
         konto = Konto(self.imie, self.nazwisko, self.pesel, None)
         konto.saldo = 100
-        konto.zaksieguj_przelew(200)
+        konto.zaksieguj_przelew_wychodzacy(200)
 
         self.assertEqual(konto.saldo, 100, "Za mało środków!")
+    
+    def test_udany_przelew_przychodzacy(self):
+        konto = Konto(self.imie, self.nazwisko, self.pesel, None)
+        konto.saldo = 1000
+        konto.zaksieguj_przelew_przychodzacy(200)
+
+        self.assertEqual(konto.saldo, 1200)
