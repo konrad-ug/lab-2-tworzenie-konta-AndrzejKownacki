bd30777aa3782a461de0e338dd71f6df607964bf
Andrzej Kownacki <akjjpol@gmail.com>
2022-11-10 17:42:51 +0100

    Paramara

--- app/tests/test_kredyty.py
+++ app/tests/test_kredyty.py
@@ -1,43 +1,26 @@
 import unittest
 
+from parameterized import parameterized
 from app.Konto import Konto
 
 class TestKredyt(unittest.TestCase):
-    imie = "Dariusz"
-    nazwisko = "Testowy"
-    pesel = "93211479876"
-
-    def test_3_przychodzace_przelewy(self):
-        konto = Konto(self.imie, self.nazwisko, self.pesel, None)
-        konto.historia = [-100, 100, 100, 100]
-        czy_przyznany = konto.zaciagnij_kredyt(500)
-        self.assertTrue(czy_przyznany)
-        self.assertEqual(konto.saldo, 500)
-
-    def test_5_w_historii(self):
-        konto = Konto(self.imie, self.nazwisko, self.pesel, None)
-        konto.historia = [100, 100, -100, 100, 1000]
-        czy_przyznany = konto.zaciagnij_kredyt(500)
-        self.assertTrue(czy_przyznany)
-        self.assertEqual(konto.saldo, 500)
     
-    def test_4_w_historii(self):
-        konto = Konto(self.imie, self.nazwisko, self.pesel, None)
-        konto.historia = [100, -100, 100, 1000]
-        czy_przyznany = konto.zaciagnij_kredyt(500)
-        self.assertTrue(czy_przyznany)
-        self.assertEqual(konto.saldo, 500)
+    @parameterized([
+        ([-100, 100, 100, 100], 500, True, 500),
+        ([-100, 100, -100, 100, 1000], 500, True, 500),
+        ([100, -100, 100, 1000], 500, True, 500),
+        ([-100, 100, 1000], 500, True, 500),
+        ([-100, 100, 100], 500, False, 0),
+    ])
 
-    def test_3_w_historii(self):
-        konto = Konto(self.imie, self.nazwisko, self.pesel, None)
-        konto.historia = [-100, 100, 1000]
-        czy_przyznany = konto.zaciagnij_kredyt(500)
-        self.assertTrue(czy_przyznany)
-        self.assertEqual(konto.saldo, 500)
+    def setUp(self):
+        imie = "Dariusz"
+        nazwisko = "Testowy"
+        pesel = "93211479876"
+        self.konto = Konto(imie, nazwisko, pesel, None)
 
-    def test_nieudany_kredyt(self):
-        konto = Konto(self.imie, self.nazwisko, self.pesel, None)
-        konto.historia = [-100, 100, 100]
-        czy_przyznany = konto.zaciagnij_kredyt(500)
-        self.assertFalse(czy_przyznany)
-        self.assertEqual(konto.saldo, 0)
+    def test_przychodzace_przelewy(self, historia, kwota, oczekiwany_wynik, saldo):
+        self.konto.historia = historia
+        czy_przyznany = self.konto.zaciagnij_kredyt(kwota)
+        self.assertEqual(czy,przyznany, oczekiwany_wynik)
+        self.assertEqual(self.konto.saldo, saldo)
