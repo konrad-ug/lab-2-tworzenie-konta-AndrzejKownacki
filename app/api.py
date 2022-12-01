57b1b23fa97bb0aa89794d0bab370749da556c31
Andrzej Kownacki <akjjpol@gmail.com>
2022-12-01 19:16:22 +0100

    CRUD poczatek

--- app/api.py
+++ app/api.py
@@ -16,8 +16,19 @@ def stworz_konto():
 def ile_kont():
     return f"Ilość kont w rejestrze {RejestrKont.ile_kont()}", 200
 
-@app.route("/konta/konto/<pesel>", methods=['GET'])
+@app.route("/konta/konto/<pesel>", methods=['GET', 'PUT'])
 def wyszukaj_konto_z_peselem(pesel):
-    konto = RejestrKont.wyszukaj_konto_z_peselem(pesel)
-    print(konto)
-    return jsonify(imie=konto.imie, nazwisko=konto.nazwisko, pesel=konto.pesel, saldo=konto.saldo), 200
+    if request.method == "GET":
+        konto = RejestrKont.wyszukaj_konto_z_peselem(pesel)
+        print(konto)
+        return jsonify(imie=konto.imie, nazwisko=konto.nazwisko, pesel=konto.pesel, saldo=konto.saldo), 200
+    elif reques.method == "PUT":
+        dane = request.get_json()
+        print(f"Update konta z peselem {pesel}")
+        RejestrKont.zaktualizuj_konto(pesel, dane)
+        return jsonify("Konto zaktualizowane"), 202
+    else:
+        RejestrKont.usun_konto(pesel)
+        return jsonify("Konto usunięte"), 202
+
+
