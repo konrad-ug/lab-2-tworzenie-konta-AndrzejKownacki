1a0f819cb69bbfbf12a5f45b92d8e83f99685a8d
Andrzej Kownacki <akjjpol@gmail.com>
2022-12-08 19:11:33 +0100

    Poczatek feature 17

--- app/KontoFirmowe.py
+++ app/KontoFirmowe.py
@@ -1,15 +1,29 @@
 from app.Konto import Konto
+from datetime import date
+import requests
+
+#https://wl-api.mf.gov.pl/api/search/nip/8461627563?date=2022-12-08
 
 class KontoFirmowe(Konto):
     def __init__(self, nazwa_firmy, nip):
         self.nazwa_firmy = nazwa_firmy
         self.saldo = 0
         self.oplata = 5
-        self.nip = nip if self.czy_poprawny_nip(nip) else "Niepoprawny NIP!"
+        self.nip = nip
         self.historia = []
 
-    def czy_poprawny_nip(self, nip):
-        return len(nip) == 10
+        if len(nip) != 10: 
+            self.nip = "Niepoprawny nip"
+        else:
+            url = "https://wl-api.mf.gov.pl/api/search/nip/"
+            data = date.today()
+            url = f"{url}{nip}?date={data}"
+
+            get_resp = requests.get(url)
+            resp_body = get_resp.json()
+
+            if resp_body['result']['subject'] == None:
+                self.nip = "Pranie!"
 
     def zaciagnij_kredyt_firma(self, kwota):
         czy_przyznany = False
